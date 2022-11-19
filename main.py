ab8db51f2b35acf7dd42b104a7a6d8c644009ad4
ivanlozhkin <116944800+ivanlozhkin@users.noreply.github.com>
2022-11-19 20:13:56 +0300

    Added index.html
    Added index.html for viewing

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
-from flask import Flask
+from flask import Flask, render_template
 
 
 app = Flask(__name__)
 
 @app.route('/url_name/<name>&<massage>')
-def url_name(name, massage):
-    return f"Hello {name}! {massage}!"
+def index(name, massage):
+    return render_template('index.html', name=name, massage=massage)
 
 
-if __name__ == '__main__':
+if __name__ == '__main__': 
     app.run()
